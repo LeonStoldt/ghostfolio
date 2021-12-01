c4294277364016bbff2c92352016e9618bb17098
Thomas <4159106+dtslvr@users.noreply.github.com>
2021-12-01 20:28:02 +0100

    Fix import

--- apps/api/src/services/exchange-rate-data.module.ts
+++ apps/api/src/services/exchange-rate-data.module.ts
@@ -1,6 +1,6 @@
-import { MarketDataService } from '@ghostfolio/api/app/portfolio/market-data.service';
 import { DataProviderModule } from '@ghostfolio/api/services/data-provider/data-provider.module';
 import { ExchangeRateDataService } from '@ghostfolio/api/services/exchange-rate-data.service';
+import { MarketDataService } from '@ghostfolio/api/services/market-data.service';
 import { Module } from '@nestjs/common';
 
 import { PrismaModule } from './prisma.module';
